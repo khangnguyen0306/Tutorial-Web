d23aed2efa6ca0974a5f102f5ac4235af503a608
SE172533_TrongKhang <chinh0726@gmail.com>
2024-09-11 16:04:13 +0700

    tetst

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,12 @@ import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { Button, Layout, theme } from 'antd'
 import Search from 'antd/es/input/Search'
 import React from 'react'
-
+import './Header.scss';
 const HeaderCustom = ({ collapsed, setCollapsed }) => {
+
     const { Header, Sider, Content } = Layout;
     const {
-        token: { colorBgContainer, borderRadiusLG,colorBgLayout  },
+        token: { colorBgContainer, borderRadiusLG, colorBgLayout },
     } = theme.useToken();
     return (
         <Header
@@ -20,13 +21,13 @@ const HeaderCustom = ({ collapsed, setCollapsed }) => {
                 justifyContent: "space-between",
                 alignItems: "center",
                 height: '80px',
-                
+
             }}
-            
+
         >
-        <div className='p-14'>
-            <p></p>
-        </div>
+            <div className='p-14'>
+                <p></p>
+            </div>
 
             <Search
                 allowClear
@@ -36,13 +37,32 @@ const HeaderCustom = ({ collapsed, setCollapsed }) => {
                 style={{
                     width: "30%",
                     borderRadius: "50%",
-                    fontSize:'14px'
+                    fontSize: '14px'
                 }}
             />
 
             <div style={{ display: "flex", gap: "10px", paddingRight: "16px" }}>
-                <Button type="primary">Đăng nhập</Button>
-                <Button>Đăng ký</Button>
+                <Button
+                    size='large'
+                    className='hover:scale-105 hover:shadow-cyan-200 hover:shadow-lg '
+                    style={{
+                        fontSize: '14px',
+                        borderRadius: '25px',
+                        padding: '0 30px'
+                    }}
+                >Đăng ký
+                </Button>
+                <Button
+                    size='large'
+                    className="
+                    bg-gradient-to-r 
+                    from-blue-500 to-cyan-400 text-white 
+                    font-medium rounded-full py-2 px-6 transition-transform duration-800
+                     hover:from-cyan-400 hover:to-blue-500 hover:scale-105 hover:shadow-cyan-200 hover:shadow-lg"
+                    type="primary"
+                >
+                    Đăng nhập
+                </Button>
             </div>
 
         </Header>
