ed729b63ce36b555e9a8c5699e36f486a18da5dc
lamquocvinh <lamvinh940@gmail.com>
2024-09-20 21:40:04 +0700

    fix-my-course

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 
-import { Button, Layout, Modal, Popover, Spin } from 'antd'
+import { Button, Layout, Modal, Popover, Spin, Progress } from 'antd'
 import Search from 'antd/es/input/Search'
 import React, { useState } from 'react'
 import './Header.scss';
@@ -20,6 +20,11 @@ const HeaderCustom = ({ collapsed }) => {
 
     const hide = () => {
         setOpen(false);
+    };
+
+    const twoColors = {
+        '0%': '#108ee9',
+        '100%': '#87d068',
     };
 
     const handleOpenChange = (newOpen) => {
@@ -89,7 +94,10 @@ const HeaderCustom = ({ collapsed }) => {
                                                 className="w-full h-full object-cover rounded-[10px]"
                                             />
                                         </div>
-                                        <span className="ml-2">{course.name}</span>
+                                        <div className="flex-1 ml-2 mr-1">
+                                            <span className="ml-2">{course.name}</span>
+                                            <Progress className="ml-2" percent={course.duration} strokeColor={twoColors} />
+                                        </div>
                                     </div>
                                 ))
                         ) : (
