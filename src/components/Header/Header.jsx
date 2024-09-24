4f0782ae48a6c4cd7f5a8c31f5e54a8a3eb756ff
lamquocvinh <lamvinh940@gmail.com>
2024-09-24 11:36:45 +0700

    progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 
-import { Button, Empty, Layout, Modal, Popover, Spin } from 'antd'
+import { Button, Empty, Layout, Modal, Popover, Spin, Progress } from 'antd'
 import Search from 'antd/es/input/Search'
 import React, { useState } from 'react'
 import './Header.scss';
@@ -21,6 +21,11 @@ const HeaderCustom = ({ collapsed }) => {
 
     const hide = () => {
         setOpen(false);
+    };
+
+    const twoColors = {
+        '0%': '#108ee9',
+        '100%': '#87d068',
     };
 
     const handleOpenChange = (newOpen) => {
@@ -80,7 +85,7 @@ const HeaderCustom = ({ collapsed }) => {
             <div style={{ display: "flex", gap: "10px", paddingRight: "16px", alignItems: 'center' }}>
                 <Popover
                     content={isLoading ? <Spin /> : (
-                        <div>
+                        <div style={{ maxHeight: '500px', overflowY: 'auto' }}>
                             {Mycourses.length > 0 ? (
                                 Mycourses
                                     .filter(course => course.isSub)
@@ -95,8 +100,11 @@ const HeaderCustom = ({ collapsed }) => {
                                                         className="w-full h-full object-cover rounded-[7px]"
                                                     />
                                                 </div>
-                                                <span className="ml-3 font-semibold">{course.name}</span>
 
+                                                <div className="flex-1 ml-2 mb-8">
+                                                    <span className="ml-2">{course.name}</span>
+                                                    <Progress className="ml-2" percent={parseInt(course.duration)} strokeColor={twoColors} />
+                                                </div>
                                             </div>
                                         </Link>
                                     ))
